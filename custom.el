eadd423e380e9ec8682ddc3d1b5623547611129a
Troy Hinckley <t.macman@gmail.com>
2020-02-17 08:51:41 -0800

    Alphabetize custom options
    This is the way things are done by default.

--- custom.el
+++ custom.el
@@ -7,6 +7,7 @@
    (quote
     ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(debug-on-error nil)
+ '(menu-bar-mode nil)
  '(safe-local-variable-values
    (quote
     ((org-src-preserve-indentation . t)
@@ -24,14 +25,13 @@
            (rainbow-mode 1))
      (bug-reference-bug-regexp . "#\\(?2:[[:digit:]]+\\)"))))
  '(savehist-additional-variables (quote (helm-ff-history)))
+ '(scroll-bar-mode nil)
  '(shx-kept-commands
    (quote
     (("show all tool config" . "ToolConfig.pl show_tool_info | ep")
      ("tool_info" . "ToolConfig.pl show_tool_info ipconfig | ep")
      ("Enable wordwrap at 90 columns" . ":eval (shx-wordwrap 90)"))))
  '(tool-bar-mode nil)
- '(menu-bar-mode nil)
- '(scroll-bar-mode nil)
  '(tooltip-mode nil))
 (put 'upcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
