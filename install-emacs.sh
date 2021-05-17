f47d854eebae45327c7d121dfb72ebcc61275030
Troy Hinckley <t.macman@gmail.com>
2021-05-17 13:31:40 -0600

    update install script for emacs 28

--- install-emacs.sh
+++ install-emacs.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 sudo apt install -y autoconf automake autotools-dev bsd-mailx build-essential \
     diffstat gnutls-dev imagemagick libasound2-dev libc6-dev libdatrie-dev \
     libdbus-1-dev libgconf2-dev libgif-dev libgnutls28-dev libgpm-dev libgtk2.0-dev \
@@ -6,13 +7,13 @@ sudo apt install -y autoconf automake autotools-dev bsd-mailx build-essential \
     librsvg2-dev libsm-dev libthai-dev libtiff5-dev libtiff-dev libtinfo-dev libtool \
     libx11-dev libxext-dev libxi-dev libxml2-dev libxmu-dev libxmuu-dev libxpm-dev \
     libxrandr-dev libxt-dev libxtst-dev libxv-dev quilt sharutils texinfo xaw3dg \
-    xaw3dg-dev xorg-dev xutils-dev zlib1g-dev libjansson-dev libwebkit2gtk-4.0-dev
+    xaw3dg-dev xorg-dev xutils-dev zlib1g-dev libjansson4 libjansson-dev libwebkit2gtk-4.0-dev \
+    libgccjit0 libgccjit-9-dev
 
-cd ~
-git clone -b emacs-27 git://git.sv.gnu.org/emacs.git emacs-git
-cd emacs-git
+git clone git://git.sv.gnu.org/emacs.git ~/emacs-git
+cd ~/emacs-git
 
 ./autogen.sh
-./configure --with-modules --with-mailutils --with-xwidgets
+./configure --with-modules --with-mailutils --with-xwidgets --with-imagemagick --with-native-compilation
 make
 sudo make install
