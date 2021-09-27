f4137a0202dbed1d4e8301d1e7f2fb1633aa4de3
Troy Hinckley <t.macman@gmail.com>
2021-09-27 11:15:12 -0600

    Update lsp custom variables

--- custom.el
+++ custom.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+   '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
  '(debug-on-error nil)
+ '(lsp-rust-analyzer-diagnostics-disabled ["unresolved-import"])
  '(menu-bar-mode nil)
  '(safe-local-variable-values
    '((eval require 'org-roam-dev)
@@ -20,17 +20,15 @@
      (cperl-close-paren-offset . -4)
      (cperl-indent-parens-as-block . t)
      (eval when
-           (fboundp
-            (quote rainbow-mode))
+           (fboundp 'rainbow-mode)
            (rainbow-mode 1))
      (bug-reference-bug-regexp . "#\\(?2:[[:digit:]]+\\)")))
- '(savehist-additional-variables (quote (helm-ff-history)))
+ '(savehist-additional-variables '(helm-ff-history))
  '(scroll-bar-mode nil)
  '(shx-kept-commands
-   (quote
-    (("show all tool config" . "ToolConfig.pl show_tool_info | ep")
+   '(("show all tool config" . "ToolConfig.pl show_tool_info | ep")
      ("tool_info" . "ToolConfig.pl show_tool_info ipconfig | ep")
-     ("Enable wordwrap at 90 columns" . ":eval (shx-wordwrap 90)"))))
+     ("Enable wordwrap at 90 columns" . ":eval (shx-wordwrap 90)")))
  '(tool-bar-mode nil)
  '(tooltip-mode nil))
 (put 'upcase-region 'disabled nil)
