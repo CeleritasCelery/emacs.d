5b81d9ea027ebcb5a8682168c9ed213ee8814a61
Troy Hinckley <t.macman@gmail.com>
2024-05-06 11:27:55 -0500

    Update stash

--- stash.el
+++ stash.el
@@ -1,42 +1,6 @@
 ;;; -*- lexical-binding: t; -*-
 
 
-(defun $compile-interactive ()
-  (interactive)
-  (compile (concat "./" (file-name-nondirectory (buffer-file-name))) t)
-  (evil-window-right 1)
-  (evil-insert-state))
-
-($leader-local-set-key
-  :keymaps 'python-mode-map
-  "x" '$compile-interactive)
-
-(use-package lsp-pyright
-  :ensure t)  ; or lsp-deferred
-
-
-(defun $read-bytecode (x)
-  (let  ((arglist (aref x 0))
-         (code (aref x 1))
-         (const (aref x 2)))
-    (vector arglist (loop for c across code collect c) const)))
-
-(use-package company-box)
-
-(with-eval-after-load 'company
-  ;; disable inline previews
-  (delq 'company-preview-if-just-one-frontend company-frontends))
-
-
-(use-package swift-mode
-  :gfhook 'copilot-mode 'lsp
-  :config
-  (setq lsp-sourcekit-executable (string-trim (shell-command-to-string "xcrun --find sourcekit-lsp"))))
-
-(use-package lsp-sourcekit
-  :demand t
-  :after 'swift-mode)
-
 (setq recentf-list (loop for file in recentf-list
                          unless (s-contains-p "archive" file)
                          collect file))
